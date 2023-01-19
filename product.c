84c553a40f6bbd930645ea73ae454253917d4f59
liyayiz41 <122539037+liyayiz41@users.noreply.github.com>
2023-01-19 16:58:10 +0300

    Add files via upload

---
+++ product.c
@@ -0,0 +1,15 @@
+/*Arithmetic operators multiplication - user friendly program*/
+#include <stdio.h>
+
+int main ()
+{
+int x,z,product;
+printf("\n Enter value of X:");
+scanf("%d",&x);
+printf("\n Enter value of Z:");
+scanf("%d",&z);
+product=x*z;
+printf("\n product=%d",product);
+return 0;
+
+}
