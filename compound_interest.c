6bf3c9bb1386a1b889259a2cb2977706959076e2
liyayiz41 <122539037+liyayiz41@users.noreply.github.com>
2023-01-28 13:22:14 +0300

    Add files via upload

---
+++ compound_interest.c
@@ -0,0 +1,20 @@
+//program to calculate compound interest
+#include<stdio.h>
+#include<math,h>
+int main()
+{
+float t,r,p,interest;
+printf("\nEnter the principal_amount:");
+scanf("%f",&p);
+
+printf("\nEnter time value:");
+scanf("%f",&t);
+
+printf("\nEnter rate value:");
+scanf("%f",&r);
+
+interest=p * pow((1+r),t);
+//pow(2,8)
+printf("interest=%7.2f",interest);
+return 0;
+}
